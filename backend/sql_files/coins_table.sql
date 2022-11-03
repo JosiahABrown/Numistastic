6f6013a5c459cb0a2703734fb372eeabb892be8b
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2022-11-03 18:13:29 -0500

    changed sql file. Added two new tables

--- backend/sql_files/coins_table.sql
+++ backend/sql_files/coins_table.sql
@@ -1,17 +1,41 @@
 	CREATE TABLE coins (
-		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		ID INT PRIMARY KEY AUTOINCREMENT,
+		numista_id INT,
 		name TEXT NOT NULL,
 		issuer TEXT NOT NULL,
-		year INT,
-		mintage TEXT,
+		years TEXT,
 		composition TEXT,
-		mint_letter TEXT,
 		description TEXT
 	);
 
+	CREATE TABLE coin_years (
+		ID INT PRIMARY KEY AUTOINCREMENT,
+		numista_id INT,
+		year INT,
+		mintage INT,
+		mint_letter TEXT,
+		description TEXT,
+		coin_id INT NOT NULL
+	);
+
+	CREATE TABLE coin_prices (
+		ID INT PRIMARY KEY AUTOINCREMENT,
+		g INT,
+		vg INT,
+		f INT,
+		vf INT,
+		xf INT,
+		au INT,
+		unc INT,
+		coin_year_id INT NOT NULL
+	);
+
 	CREATE TABLE collection (
-		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		ID INT PRIMARY KEY AUTOINCREMENT,
 		coin_id INT NOT NULL,
 		condition TEXT,
+		tag TEXT,
+		image BLOB,
+		buy_price INT,
 		note TEXT
 	);
